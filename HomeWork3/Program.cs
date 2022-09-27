decc402dc779e7cce80a8343aff80aad7fa2c870
katescha8 <kate.schapo.5@gmail.com>
2022-09-27 21:40:55 +0300

    Классная работа 4.1, 4.2

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
